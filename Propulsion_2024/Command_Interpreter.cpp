13dd396dc1b3ab3b82f34743ec4080feba7b2bc2
William <107134814+WilliamBarber@users.noreply.github.com>
2024-11-14 21:12:49 -0800

    Update Command_Interpreter.cpp
    Begin work on Command Interpreter class implementation

--- Propulsion_2024/Command_Interpreter.cpp
+++ Propulsion_2024/Command_Interpreter.cpp
@@ -1,1 +1,10 @@
+// William Barber
 #include "Command_Interpreter.h"
+#include <wiringPi.h> // this needs to be installed on the pi
+
+void Command_Interpreter_RPi5::initializePins() {
+    wiringPiSetupGpio();
+    for (Pin pin : pins) {
+        pinMode(pin.gpioNumber, OUTPUT); // OUTPUT may be PWM_OUTPUT
+    }
+}
