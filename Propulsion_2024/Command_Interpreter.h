65d3ee59c44e4ca819b4de74612107a83951fd68
William <107134814+WilliamBarber@users.noreply.github.com>
2024-11-14 21:12:09 -0800

    Update Command_Interpreter.h
    Update header for Command Interpreter class, create Pin class header.

--- Propulsion_2024/Command_Interpreter.h
+++ Propulsion_2024/Command_Interpreter.h
@@ -1,12 +1,31 @@
 #pragma once
 #include "command.h"
+#include <vector>
 
- // The purpose of this class is toggle the GPIO pins on the Raspberry Pi bassed on a command object
+// The purpose of this class is toggle the GPIO pins on the Raspberry Pi based on a command object
 // Requires information about wiring, ect.
+enum PinStatus {Enabled, Disabled};
+enum EnableType {ActiveHigh, ActiveLow};
 
-class Command_Interpreter_RPi5
-{
-	void execute(Command command);
-
+class Pin {
+private:
+    int gpioNumber;
+    PinStatus pinStatus;
+    EnableType enableType;
+public:
+    Pin();
+    Pin(int gpioNumber);
+    Pin(int gpioNumber, EnableType enableType);
+    void enable();
+    void disable(); //if we are using PWM, we'll want to change this to "set pwm value" rather than enable/disable
+    friend class Command_Interpreter_RPi5;
 };
 
+class Command_Interpreter_RPi5 {
+private:
+    std::vector<Pin> pins;
+public:
+    Command_Interpreter_RPi5();
+    void execute(Command command);
+    void initializePins();
+};
