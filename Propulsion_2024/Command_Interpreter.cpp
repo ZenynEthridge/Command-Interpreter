52a9f131485e56f9d37569e0cea01758dddafad2
William <107134814+WilliamBarber@users.noreply.github.com>
2024-11-14 21:31:23 -0800

    Update Command_Interpreter.cpp
    Add function definitions for functions defined in header file

--- Propulsion_2024/Command_Interpreter.cpp
+++ Propulsion_2024/Command_Interpreter.cpp
@@ -1,6 +1,46 @@
 // William Barber
 #include "Command_Interpreter.h"
 #include <wiringPi.h> // this needs to be installed on the pi
+#include <iostream>
+#include <utility>
+
+Pin::Pin(int gpioNumber): gpioNumber(gpioNumber), enableType(ActiveLow), pinStatus(Disabled) {}
+Pin::Pin(int gpioNumber, EnableType enableType): gpioNumber(gpioNumber), enableType(enableType), pinStatus(Disabled) {}
+
+void Pin::enable() {
+    switch (enableType) {
+        case ActiveHigh:
+            digitalWrite(gpioNumber, HIGH);
+            break;
+        case ActiveLow:
+            digitalWrite(gpioNumber, LOW);
+            break;
+        default:
+            std::cerr << "Unknown pin mode!" << std::endl;
+    }
+    pinStatus = Enabled;
+}
+
+void Pin::disable() {
+    switch (enableType) {
+        case ActiveHigh:
+            digitalWrite(gpioNumber, LOW);
+            break;
+        case ActiveLow:
+            digitalWrite(gpioNumber, HIGH);
+            break;
+        default:
+            std::cerr << "Unknown pin mode!" << std::endl;
+    }
+    pinStatus = Disabled;
+}
+
+bool Pin::enabled() {
+    return pinStatus == Enabled;
+}
+
+Command_Interpreter_RPi5::Command_Interpreter_RPi5(): pins(std::vector<Pin>{}) {}
+Command_Interpreter_RPi5::Command_Interpreter_RPi5(std::vector<Pin> pins): pins(std::move(pins)) {}
 
 void Command_Interpreter_RPi5::initializePins() {
     wiringPiSetupGpio();
