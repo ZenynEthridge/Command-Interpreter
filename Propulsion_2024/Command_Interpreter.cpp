a7a144933645bb738e89cb049486f7ee9e5783b1
WilliamBarber <wobarber@outlook.com>
2024-11-14 23:11:42 -0800

    Update Command_Interpreter
    Refactored Pin into abstract class. Created DigitalPin and PwmPin to extend Pin.

--- Propulsion_2024/Command_Interpreter.cpp
+++ Propulsion_2024/Command_Interpreter.cpp
@@ -2,12 +2,15 @@
 #include "Command_Interpreter.h"
 #include <wiringPi.h> // this needs to be installed on the pi
 #include <iostream>
-#include <utility>
 
-Pin::Pin(int gpioNumber): gpioNumber(gpioNumber), enableType(ActiveLow), pinStatus(Disabled) {}
-Pin::Pin(int gpioNumber, EnableType enableType): gpioNumber(gpioNumber), enableType(enableType), pinStatus(Disabled) {}
 
-void Pin::enable() {
+DigitalPin::DigitalPin(int gpioNumber, EnableType enableType): Pin(gpioNumber), pinStatus(Disabled), enableType(enableType) {}
+
+void DigitalPin::initialize() {
+    pinMode(gpioNumber, OUTPUT);
+}
+
+void DigitalPin::enable() {
     switch (enableType) {
         case ActiveHigh:
             digitalWrite(gpioNumber, HIGH);
@@ -21,7 +24,7 @@ void Pin::enable() {
     pinStatus = Enabled;
 }
 
-void Pin::disable() {
+void DigitalPin::disable() {
     switch (enableType) {
         case ActiveHigh:
             digitalWrite(gpioNumber, LOW);
@@ -35,16 +38,48 @@ void Pin::disable() {
     pinStatus = Disabled;
 }
 
-bool Pin::enabled() {
+bool DigitalPin::enabled() {
     return pinStatus == Enabled;
 }
+
+
+PwmPin::PwmPin(int gpioNumber): Pin(gpioNumber), currentPwm(0) {}
+
+void PwmPin::initialize() {
+    pinMode(gpioNumber, PWM_OUTPUT);
+}
+
+void PwmPin::enable() {
+    setPowerAndDirection(255, Forwards);
+}
+
+void PwmPin::disable() {
+    setPowerAndDirection(0, Forwards);
+}
+
+bool PwmPin::enabled() {
+    return currentPwm != 0;
+}
+
+void PwmPin::setPowerAndDirection(int pwmValue, Direction direction) {
+    //TODO: set direction (not sure how to do this w/ pwm. High/low?)
+    pwmWrite(gpioNumber, pwmValue);
+}
+
+
 
 Command_Interpreter_RPi5::Command_Interpreter_RPi5(): pins(std::vector<Pin>{}) {}
 Command_Interpreter_RPi5::Command_Interpreter_RPi5(std::vector<Pin> pins): pins(std::move(pins)) {}
 
 void Command_Interpreter_RPi5::initializePins() {
-    wiringPiSetupGpio();
-    for (Pin pin : pins) {
-        pinMode(pin.gpioNumber, OUTPUT); // OUTPUT may be PWM_OUTPUT
+    if (wiringPiSetupGpio() == -1) {
+        std::cerr << "Failure to configure GPIO pins through wiringPi!" << std::endl;
     }
+    for (Pin& pin : pins) {
+        pin.initialize();
+    }
+}
+
+void Command_Interpreter_RPi5::execute(Command command) {
+    //TODO
 }
--- Propulsion_2024/Command_Interpreter.h
+++ Propulsion_2024/Command_Interpreter.h
@@ -6,19 +6,41 @@
 // Requires information about wiring, ect.
 enum PinStatus {Enabled, Disabled};
 enum EnableType {ActiveHigh, ActiveLow};
+enum Direction {Forwards, Backwards};
 
 class Pin {
-private:
-    int gpioNumber;
-    PinStatus pinStatus;
-    EnableType enableType;
+protected:
+    int gpioNumber{};
 public:
-    Pin(int gpioNumber);
-    Pin(int gpioNumber, EnableType enableType);
-    void enable();
-    void disable(); //if we are using PWM, we'll want to change this to "set pwm value" rather than enable/disable
-    bool enabled();
-    friend class Command_Interpreter_RPi5;
+    virtual void initialize()= 0;
+    virtual void enable() = 0;
+    virtual void disable() = 0;
+    virtual bool enabled() = 0;
+    explicit Pin(int gpioNumber) : gpioNumber(gpioNumber) {}
+};
+
+class DigitalPin : public Pin {
+private:
+    EnableType enableType;
+    PinStatus pinStatus;
+public:
+    void initialize() override;
+    void enable() override;
+    void disable() override;
+    bool enabled() override;
+    DigitalPin(int gpioNumber, EnableType enableType);
+};
+
+class PwmPin : public Pin {
+private:
+    int currentPwm;
+public:
+    void initialize() override;
+    void enable() override;
+    void disable() override;
+    bool enabled() override;
+    void setPowerAndDirection(int pwmValue, Direction direction);
+    explicit PwmPin(int gpioNumber);
 };
 
 class Command_Interpreter_RPi5 {
