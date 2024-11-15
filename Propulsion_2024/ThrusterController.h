2fb3282a2810864f813f3bdb7f981c44997f6ec5
Zenyn <ZJEthridge@UCDavis.edu>
2024-11-14 19:42:38 -0800

    added drag force prototypes

--- Propulsion_2024/ThrusterController.h
+++ Propulsion_2024/ThrusterController.h
@@ -21,15 +21,27 @@ protected:
 	int num_thrusters; // Number of thrusters on the vehicle
 	float mass;       // Mass of the vehicle (kg)
 	float volume;    // Displacement volume of the vehicle (m^3)
+	float rho_water = 1025; // Density of water (kg/m^3)
 
 	
 	// Returns the PWM value for a given thruster and force
 	int get_pwm(int thruster_num, float force);
 
+
+	// Drag force = 0.5 * rho_water * v^2 * Cd * A
+	// rho water is known, A can be found as a function of direction, v is input, Cd is unknown, but can be estimated or found through trail and error
+	Eigen::Matrix<float, 1, 3> predict_drag_forces(
+		float x_velocity, float y_velocity, float z_velocity);
+	
+	// <drag_torque> = <r> x <drag_force> = <r> x < 0.5 * rho_water * (r*omega)^2 * Cd * A >
+	// this will be a little trickier to calculate, might require integration
+	Eigen::Matrix<float, 1, 3> predict_drag_torques(
+		float x_velocity, float y_velocity, float z_velocity, float x_angular_velocity, 
+		float y_angular_velocity, float z_angular_velocity);
+
 public:
 	Thruster_Commander();
 	~Thruster_Commander();
-	
 
 	// mostly for debugging purposes
 	void print_info();
