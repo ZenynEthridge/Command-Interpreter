d120a3cc93dc4b7cd2d655eac4004ddf9c02199b
Zenyn <ZJEthridge@UCDavis.edu>
2024-11-16 18:13:22 -0800

    fixed some compile errors

--- Propulsion_2024/Thruster_Commander.cpp
+++ Propulsion_2024/Thruster_Commander.cpp
@@ -122,15 +122,20 @@ void Thruster_Commander::print_info()
 //	{
 //		pwm_array[i]= resultingPWMfromForce[i];
 //	}
-	
+	 pwm_array pwm_signals;
+	 return pwm_signals;
 }
 pwm_array Thruster_Commander::simple_forward(float force){
-	
+	pwm_array pwm_signals;
+	return pwm_signals;
 }
 pwm_array Thruster_Commander::simple_sideways(float force){
+	pwm_array pwm_signals;
+	return pwm_signals;
 	
 }
 pwm_array Thruster_Commander::simple_horizontal(float x_force, float y_force)
 {
-
+	pwm_array pwm_signals;
+	return pwm_signals;
 }
