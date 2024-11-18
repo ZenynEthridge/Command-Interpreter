86bf49071095d51187c65c3a3f5a1407c1ba1da7
Zenyn <ZJEthridge@UCDavis.edu>
2024-11-17 17:14:19 -0800

    commented out unit testing in main block

--- Propulsion_2024/Main.cpp
+++ Propulsion_2024/Main.cpp
@@ -16,19 +16,19 @@ int main() {
 	Thruster.print_info();
 
     //Begin command/command interpreter testing
-    struct Command command = {1500, Forwards, 1900, Backwards, 1100, Forwards, 1500, Forwards, 1900, Backwards, 1100, Forwards, 1500, Forwards, 1900, Backwards, 2};
-    auto pin1 = new PwmPin(0);
-    auto pin2 = new PwmPin(1);
-    auto pin3 = new PwmPin(2);
-    auto pin4 = new PwmPin(3);
-    auto pin5 = new PwmPin(4);
-    auto pin6 = new PwmPin(5);
-    auto pin7 = new PwmPin(6);
-    auto pin8 = new PwmPin(7);
-    auto pins = std::vector<PwmPin*>{pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8};
-    auto interpreter = new Command_Interpreter_RPi5(pins, std::vector<DigitalPin*>{});
-    interpreter->execute(command);
-    delete interpreter;
+    //struct Command command = {1500, Forwards, 1900, Backwards, 1100, Forwards, 1500, Forwards, 1900, Backwards, 1100, Forwards, 1500, Forwards, 1900, Backwards, 2};
+    //auto pin1 = new PwmPin(0);
+    //auto pin2 = new PwmPin(1);
+    //auto pin3 = new PwmPin(2);
+    //auto pin4 = new PwmPin(3);
+    //auto pin5 = new PwmPin(4);
+    //auto pin6 = new PwmPin(5);
+    //auto pin7 = new PwmPin(6);
+    //auto pin8 = new PwmPin(7);
+    //auto pins = std::vector<PwmPin*>{pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8};
+    //auto interpreter = new Command_Interpreter_RPi5(pins, std::vector<DigitalPin*>{});
+    //interpreter->execute(command);
+    //delete interpreter;
     //End command/command interpreter testing
 
     return 0;
