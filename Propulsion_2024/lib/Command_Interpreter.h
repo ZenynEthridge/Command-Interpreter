c8e59a88ced40f635677edf5a1e126ff83f65f81
WilliamBarber <wobarber@outlook.com>
2024-11-18 23:21:21 -0800

    Add documentation, make initialize private
    Added documentation to methods in Command_Interpreter, and made initialize() private because initialize() should only need to be called by Command_Interpreter

--- Propulsion_2024/lib/Command_Interpreter.h
+++ Propulsion_2024/lib/Command_Interpreter.h
@@ -3,10 +3,12 @@
 #include "Command.h"
 #include <vector>
 
-// The purpose of this class is toggle the GPIO pins on the Raspberry Pi based on a command object
-// Requires information about wiring, ect.
+/// @brief Whether a pin is currently in an enabled (powered) or disabled (unpowered) state
 enum PinStatus {Enabled, Disabled};
+
+///@brief Whether a digital pin is active high or active low
 enum EnableType {ActiveHigh, ActiveLow};
+
 const int MAX_PWM_VALUE = 1023; //TODO: check pwm range (currently assuming 0-1023)
 
 /*
@@ -14,17 +16,29 @@ const int MAX_PWM_VALUE = 1023; //TODO: check pwm range (currently assuming 0-10
  * be replaced with just the PwmPin class (probably renamed to Pin). This would also necessitate the removal of allPins
  * and digitalPins data members from Command_Interpreter_RPi5
  */
+
+/// @brief A Raspberry Pi 5 GPIO pin, as specified by its GPIO pin number (see https://pinout.xyz)
 class Pin {
 protected:
     int gpioNumber{};
 public:
+    /// @brief Initializes pin through WiringPi (i.e. to output, PWM, etc.)
     virtual void initialize()= 0;
+
+    /// @brief Sets pin to maximum (positive) power
     virtual void enable() = 0;
+
+    /// @brief Sets pin to be unpowered (stopped)
     virtual void disable() = 0;
+
+    /// @brief Whether a pin is currently powered (whether its power level is not zero)
+    /// @return True if power is not zero, false otherwise
     virtual bool enabled() = 0;
     explicit Pin(int gpioNumber) : gpioNumber(gpioNumber) {}
+    virtual ~Pin() = default;
 };
 
+/// @brief A digital (two-state) Raspberry Pi 5 GPIO pin
 class DigitalPin : public Pin {
 private:
     EnableType enableType;
@@ -37,6 +51,7 @@ public:
     DigitalPin(int gpioNumber, EnableType enableType);
 };
 
+/// @brief a pwm-capable Raspberry Pi 5 GPIO pin (supports analogue output)
 class PwmPin : public Pin {
 private:
     int currentPwm;
@@ -45,12 +60,19 @@ public:
     void enable() override;
     void disable() override;
     bool enabled() override;
+
+    /// @brief Sets pin to the specified pwm value and direction
+    /// @param pwmValue a pwm value between 0 and 1023
+    /// @param direction which direction the motor should spin (Forwards or Backwards)
     void setPowerAndDirection(int pwmValue, Direction direction);
     explicit PwmPin(int gpioNumber);
 };
 
+/// @brief The purpose of this class is toggle the GPIO pins on the Raspberry Pi based on a command object.
+/// Requires information about wiring, etc.
 class Command_Interpreter_RPi5 {
 private:
+    /// @brief The spec for a thruster command to be sent to GPIO
     struct ThrusterSpec {
         int pwm_value;
         Direction direction;
@@ -58,9 +80,21 @@ private:
     std::vector<Pin*> allPins;
     std::vector<PwmPin*> thrusterPins;
     std::vector<DigitalPin*> digitalPins;
+
+    /// @brief Initializes the pins in allPins through the WiringPi library to be either PWM or digital pins
+    /// depending on their types
+    void initializePins();
+
+    /// @brief Converts a pwm frequency between 1100 and 1900 into a Thruster Spec.
+    /// @param pwmFrequency a pwm frequency between 1100 and 1900
+    /// @return A Thruster Spec with a pwm magnitude between 0 and MAX_PWM_VALUE and a Direction
     static ThrusterSpec convertPwmValue(int pwmFrequency);
 public:
     explicit Command_Interpreter_RPi5(std::vector<PwmPin*> thrusterPins, std::vector<DigitalPin*> digitalPins);
+
+    /// @brief Executes a command by sending the specified pwm values to the Pi's GPIO for the specified duration
+    /// @param command a command struct with a C-style array of pwm frequency integers and a duration float
     void execute(const Command& command);
-    void initializePins();
+
+    ~Command_Interpreter_RPi5(); //TODO this also deletes all its pins. Not sure if this is desirable or not?
 };
