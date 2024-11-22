14213cf1a3eab7716ee5a725f25c7cfba3221b4f
Zenyn <ZJEthridge@UCDavis.edu>
2024-11-21 22:52:56 -0800

    Revert "merge errors corrected"
    This reverts commit a5117e1e66c50ba72ac4a0a1af630b13e657610a.

--- Propulsion_2024/lib/Main.cpp
+++ Propulsion_2024/lib/Main.cpp
@@ -1,11 +1,5 @@
-#include <iostream>
-#include "Thruster_Commander.h"
-
 int main()
 {
-	std::cout << "Running main" << std::endl;
-	Thruster_Commander thruster_commander = Thruster_Commander();
-	thruster_commander.print_info();
     return 0;
 }
 
