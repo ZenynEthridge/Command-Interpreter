546001f64d8b646ab2a237d71c88418a01ab30ae
WilliamBarber <wobarber@outlook.com>
2024-11-23 15:25:55 -0800

    Add YAML
    Added YAML to CMakeLists.txt, basic test to Main.cpp (commented out because we don't have an actual file).

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -2,17 +2,18 @@
 #include "Thruster_Commander.h"
 #include "eigen-3.4.0/Eigen/Dense"
 #include "eigen-3.4.0/Eigen/Geometry"
+#include <yaml-cpp/yaml.h>
 
 int main()
 {
-	/*Thruster_Commander control = Thruster_Commander();
-	control.print_info();
-	control.predict_drag_forces({0.5, 0.5, 0.1, 0.1, 0.1, 0.1});
-	control.weight_force({ 3.14/2, 0, 0 });
-	control.bouyant_force({ 3.14/2, 0, 0 });
-	control.graviational_forces({ 3.14 / 2, 0, 0 });
-	control.net_env_forces({ 0.5, 0.5, 0, 0, 0, 0 }, { 3.14/2, 0, 0 });
-	control.thrust_compute_fz(5);*/
+    auto data_file = YAML::LoadFile("jfdklfjlkdsf");
+    /*Thruster_Commander control = Thruster_Commander();
+    control.print_info();
+    control.predict_drag_forces({0.5, 0.5, 0.1, 0.1, 0.1, 0.1});	control.weight_force({ 3.14/2, 0, 0 });
+    control.bouyant_force({ 3.14/2, 0, 0 });
+    control.graviational_forces({ 3.14 / 2, 0, 0 });
+    control.net_env_forces({ 0.5, 0.5, 0, 0, 0, 0 }, { 3.14/2, 0, 0 });
+    control.thrust_compute_fz(5);*/
     return 0;
 }
 
