05229153669f2f45614869269519dcba0d64bfe4
Zenyn <ZJEthridge@UCDavis.edu>
2024-11-28 19:33:53 -0800

    fixed oversized array in physics testing

--- testing/Physics_Testing.cpp
+++ testing/Physics_Testing.cpp
@@ -6,12 +6,12 @@ TEST(PhysicsTest, accel_time) {
 
 	float cd = 40;
 	float m = 5;
-	float F[6] =  { 30,  50, -30, 0, 0 };
-	float v0[6] = { 0,   0,    0, 5, 1 };
-	float vt[6] = { 0.5, 0,    1, 1, 5 };
-	float t[6];
+	float F[5] =  { 30,  50, -30, 0, 0 };
+	float v0[5] = { 0,   0,    0, 5, 1 };
+	float vt[5] = { 0.5, 0,    1, 1, 5 };
+	float t[5];
 	
-	for (int i = 0; i < 6; i++) { t[i] = accel_time(v0[i], vt[i], cd, m, F[i]); }
+	for (int i = 0; i < 5; i++) { t[i] = accel_time(v0[i], vt[i], cd, m, F[i]); }
 
 	ASSERT_TRUE(t[0] > 0);
 	ASSERT_TRUE(t[1] == 0);
