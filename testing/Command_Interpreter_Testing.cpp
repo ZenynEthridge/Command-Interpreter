ccf143b77a3a4ba1eb2e5116a111a18ee47eb797
WilliamBarber <wobarber@outlook.com>
2025-04-17 09:59:18 -0700

    Update Pin 4 to more interesting test values

--- testing/Command_Interpreter_Testing.cpp
+++ testing/Command_Interpreter_Testing.cpp
@@ -74,7 +74,7 @@ TEST(CommandInterpreterTest, CreateCommandInterpreterWithDigitalPins) {
 TEST(CommandInterpreterTest, BlindExecuteHardwarePwm) {
     testing::internal::CaptureStdout();
 
-    const CommandComponent acceleration = {1500, 1900, 1100,
+    const CommandComponent acceleration = {1900, 1900, 1100,
                                      1250, 1300, 1464, 1535,
                                      1536, std::chrono::milliseconds(2000)};
 
@@ -106,7 +106,7 @@ TEST(CommandInterpreterTest, BlindExecuteHardwarePwm) {
         expectedOutput.append(std::to_string(pinNumber));
         expectedOutput.append(" PWM 1500\n");
     }
-    expectedOutput.append("Set 4 PWM 1500\n");
+    expectedOutput.append("Set 4 PWM 1900\n");
     expectedOutput.append("Set 5 PWM 1900\n");
     expectedOutput.append("Set 2 PWM 1100\n");
     expectedOutput.append("Set 3 PWM 1250\n");
@@ -117,14 +117,14 @@ TEST(CommandInterpreterTest, BlindExecuteHardwarePwm) {
 
     ASSERT_NEAR((endTime - startTime) / std::chrono::milliseconds(1), std::chrono::milliseconds(2000) /
         std::chrono::milliseconds(1), std::chrono::milliseconds(10) / std::chrono::milliseconds(1));
-    ASSERT_EQ(pinStatus, (std::vector<int>{1500, 1900, 1100, 1250, 1300, 1464, 1535, 1536}));
+    ASSERT_EQ(pinStatus, (std::vector<int>{1900, 1900, 1100, 1250, 1300, 1464, 1535, 1536}));
     ASSERT_EQ(output, expectedOutput);
 }
 
 TEST(CommandInterpreterTest, BlindExecuteSoftwarePwm) {
     testing::internal::CaptureStdout();
 
-    const CommandComponent acceleration = {1500, 1900, 1100,
+    const CommandComponent acceleration = {1100, 1900, 1100,
                                            1250, 1300, 1464, 1535,
                                            1536, std::chrono::milliseconds(2000)};
 
@@ -156,7 +156,7 @@ TEST(CommandInterpreterTest, BlindExecuteSoftwarePwm) {
         expectedOutput.append(std::to_string(pinNumber));
         expectedOutput.append(" PWM 1500\n");
     }
-    expectedOutput.append("Set 4 PWM 1500\n");
+    expectedOutput.append("Set 4 PWM 1100\n");
     expectedOutput.append("Set 5 PWM 1900\n");
     expectedOutput.append("Set 2 PWM 1100\n");
     expectedOutput.append("Set 3 PWM 1250\n");
@@ -168,7 +168,7 @@ TEST(CommandInterpreterTest, BlindExecuteSoftwarePwm) {
     ASSERT_NEAR((endTime - startTime) / std::chrono::milliseconds(1), std::chrono::milliseconds(2000) /
         std::chrono::milliseconds(1), std::chrono::milliseconds(10) / std::chrono::milliseconds(1));
 
-    ASSERT_EQ(pinStatus, (std::vector<int>{1500, 1900, 1100, 1250, 1300, 1464, 1535, 1536}));
+    ASSERT_EQ(pinStatus, (std::vector<int>{1100, 1900, 1100, 1250, 1300, 1464, 1535, 1536}));
     ASSERT_EQ(output, expectedOutput);
 }
 
